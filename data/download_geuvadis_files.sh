02d1504237d457fa0ca7102ecaf2668acc665067
francosimonetti <francolsimonetti@gmail.com>
2023-05-23 13:22:58 -0300

    download fastq.gz locally

---
+++ data/download_geuvadis_files.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+mypath="/mnf/efs/fs1/rnaseq/"
+mypath="/data/franco/rnaseq/"
+
+
+for f in `awk '{print $2}' $mypath/data/read_paths_GEUVADIS_GBR20.tsv`; do
+    echo wget $f;
+    echo \$HOME/miniconda/bin/aws s3 cp $( basename "$f" ) s3://fsimonetti-nfdata/fastqs/
+    echo -e "$( basename "$f" | sed 's/.fastq.gz//')\ts3://fsimonetti-nfdata/fastqs/$( basename "$f" )" >> new_paths_fastq1
+done;
+
+for f in `awk '{print $3}' $mypath/data/read_paths_GEUVADIS_GBR20.tsv`; do
+    echo wget $f;
+    echo \$HOME/miniconda/bin/aws s3 cp $( basename "$f" ) s3://fsimonetti-nfdata/fastqs/
+    echo -e "s3://fsimonetti-nfdata/fastqs/$( basename "$f" )" >> new_paths_fastq2
+done;
+
+paste new_paths_fastq1 new_paths_fastq2 >> s3_paths.txt
